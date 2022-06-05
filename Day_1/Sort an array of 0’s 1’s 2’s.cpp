c23712cfa942e023f355e6d234a1ff82298ef4e7
Suraj Misal <35091245+misals@users.noreply.github.com>
2022-06-05 17:31:18 +0530

    Create Sort an array of 0’s 1’s 2’s.cpp

---
+++ Day_1/Sort an array of 0’s 1’s 2’s.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        int mid = 0;
+        while(mid <= r) {
+            if(nums[mid] == 0) {
+                swap(nums[mid], nums[l]);
+                l++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else if(nums[mid] == 2) {
+                swap(nums[mid], nums[r]);
+                r--;
+            }
+        }
+        
+    }
+};
